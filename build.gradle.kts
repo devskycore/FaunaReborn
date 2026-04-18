907abe19325a277cb2e8bdf10c28df6f1a4d8af5
DevSkyCore <devskycore@gmail.com>
2026-04-18 19:58:54 -0300

    Initial commit: setup FaunaReborn project with Paper API and Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.run.paper)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    runServer {
+        minecraftVersion(libs.versions.minecraft.get())
+        jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
+    }
+
+    processResources {
+        val props = mapOf("version" to version, "description" to project.description)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/io/github/devskycore/faunareborn/FaunaRebornPlugin.java
@@ -0,0 +1,16 @@
+package io.github.devskycore.faunareborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FaunaRebornPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
